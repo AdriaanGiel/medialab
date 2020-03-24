f0ab1ca8e3fcb5bd0498306ad7085745110224ad
Dion <0949000@hr.nl>
2020-03-24 15:17:37 +0100

    Intro en brainstorm is nog leeg, sommige styling doen het nog niet

--- src/js/mainContent.vue
+++ src/js/mainContent.vue
@@ -1,13 +1,22 @@
 <template>
     <section id="content">
         <div class="container">
+            <intro-section/>
+<!--            <brainstorm-section/>-->
         </div>
     </section>
 </template>
 
 <script>
+
+    import brainstormSection from './sections/brainstorm-section'
+    import introSection from './sections/intro-section'
     export default {
-        name: "mainContent"
+        name: "mainContent",
+        components:{
+            "brainstorm-section" : brainstormSection,
+            "intro-section" : introSection
+        }
     }
 </script>
 
